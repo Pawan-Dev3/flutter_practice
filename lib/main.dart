2ad32653ca96cefa4ce71b286e57aed2dfec0124
Pawan Dev <pdev4276@gmail.com>
2023-08-02 15:52:09 +0530

    daynew1 complete

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:classico/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // int days = 30;
+    // String name = "Codepur";
+    // double pi = 3.14;
+    // bool isMale = true;
+    // num temp = 30.5;
+
+    // var day = "Tuesday";
+    // const i = 3.14;
+    // final
+
+
+    return MaterialApp(
+        home: HomePage(),
+     );
+  }
+}
